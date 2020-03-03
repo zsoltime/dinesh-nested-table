79eb18fb27ef63ff12db99116fd67bf9c189413b
Zsolt Meszaros <zsoltime@gmail.com>
2020-03-03 11:26:02 +0000

    Update sort
    - convert TableCells.jsx to dumb component
    - move sort to Table.jsx
    - add buttons to th elements
    - provide some extra info for screen readers

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,69 +1,76 @@
-import React, { Component } from "react";
-import TableCell from "../components/TableCells";
-import { getData, getHeader } from "../services/tableData";
-import Pagination from "../components/Pagination";
-import { pageData } from "../utils/pageData";
-import _ from "lodash";
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import TableCell from '../components/TableCells';
+import Pagination from '../components/Pagination';
+import { getData, getHeader } from '../services/tableData';
+import { pageData } from '../utils/pageData';
 
 class Table extends Component {
   state = {
+    currentPage: 1,
     data: [],
     header: [],
     pagesize: 4,
-    currentPage: 1,
-    sortColumn: { path: "companyName", order: "asc" }
+    sort: { field: 'id', order: 'asc' },
   };
+
   componentDidMount() {
     this.setState({ data: getData(), header: getHeader() });
   }
+
   handlePageChange = page => {
     this.setState({ currentPage: page });
   };
-  handleSort = sortColumn => {
-    const storeData = [...this.state.data];
-    console.log("storedata : ", storeData);
-    const sortData = _.orderBy(
-      storeData,
-      [sortColumn.path],
-      [sortColumn.order]
-    );
-    console.log("sortData : ", sortData);
-    this.setState({ storeData: sortData, sortColumn });
+
+  handleSort = field => {
+    // You should handle sorting only here, not in TableCells
+    this.setState(prevState => {
+      const { data, sort } = prevState;
+      // We can create a new object, the field must be the
+      // field provided and we can add an order property
+      // below, just like you did before
+      const updatedSort = { field };
+
+      if (sort.field === field) {
+        updatedSort.order = sort.order === 'asc' ? 'desc' : 'asc';
+      } else {
+        updatedSort.order = 'asc';
+      }
+
+      // Ordering the data should also be done here, not in
+      // render() as render should only read the state and
+      // render changes :)
+      const sortedData = _.orderBy(data, updatedSort.field, updatedSort.order);
+
+      return {
+        data: sortedData,
+        sort: updatedSort,
+      };
+    });
   };
-  handleSort = sortColumn => {
-    const storeData = { ...this.state.sortColumn };
-    const sortData = _.orderBy(
-      storeData,
-      [sortColumn.path],
-      [sortColumn.order]
-    );
-    this.setState({ storeData: sortData, sortColumn });
-  };
+
   render() {
-    const {
-      data: allData,
-      header,
-      pagesize,
-      currentPage,
-      sortColumn
-    } = this.state;
-    const { length: count } = this.state.data;
-    const sorted = _.orderBy(allData, [sortColumn.path], [sortColumn.order]);
-    const dataz = pageData(sorted, currentPage, pagesize);
+    const { currentPage, data, header, pagesize, sort } = this.state;
+    // I'd still move it outside, either pass currentPage,
+    // pagesize to TableCell and handle it there, or add a
+    // new paginatedData state here?
+    const paginatedData = pageData(data, currentPage, pagesize);
+
     return (
       <div className="row m-5">
         <div className="col">
           <TableCell
-            data={dataz}
+            data={paginatedData}
             header={header}
             onSort={this.handleSort}
-            sortColumn={sortColumn}
+            sort={sort}
           />
           <Pagination
-            pagesize={pagesize}
             currentPage={currentPage}
+            itemsCount={data.length}
             onPageChange={this.handlePageChange}
-            itemsCount={count}
+            pagesize={pagesize}
           />
         </div>
       </div>
--- src/components/TableCells.jsx
+++ src/components/TableCells.jsx
@@ -1,64 +1,114 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
-class TableCells extends Component {
-  sortIt = path => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc';
-    }
-    this.props.onSort(sortColumn);
-  };
-  render() {
-    const { data, header } = this.props;
-    return (
-      <div>
-        <table className="table table-bordered table-sm">
-          <thead className="thead-dark">
+function SortButton({ field, label, onSort, sort }) {
+  const isActive = sort.field === field;
+
+  return (
+    <button
+      className={`btn btn-sm btn-outline-${isActive ? 'primary' : 'secondary'}`}
+      style={{ marginLeft: '1rem' }}
+      onClick={() => {
+        onSort(field);
+      }}
+    >
+      {/* Provide some extra info for screen readers only */}
+      {/* and add a line or arrow icon for sighted users */}
+      {/* to display current order */}
+      {isActive ? (
+        <Fragment>
+          <span className="sr-only">
+            sort by {label} in{' '}
+            {`${sort.order === 'asc' ? 'desc' : 'asc'}ending`} order
+          </span>
+          <span role="img" alt="">
+            {sort.order === 'asc' ? '↓' : '↑'}
+          </span>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <span className="sr-only">sort by {label} in ascending order</span>
+          <span role="img" alt="">
+            ―
+          </span>
+        </Fragment>
+      )}
+    </button>
+  );
+}
+
+// It should be a dumb component, it doesn't need to have
+// its own state. I also think TableCells is not a great
+// name, this one should be named Table as this is the
+// table, not just the cells :)
+function TableCells({ data, header, onSort, sort }) {
+  return (
+    <table className="table table-bordered table-sm table-responsive-sm">
+      <thead className="thead-dark">
+        <tr>
+          <th
+            scope="col"
+            aria-sort={sort.field === 'id' ? `${sort.order}ending` : 'none'}
+          >
+            {header.id}
+            {/* You should use a button to handle these clicks */}
+            {/* not the table head directly. #accessibility */}
+            <SortButton
+              field="id"
+              label={header.id}
+              onSort={onSort}
+              sort={sort}
+            />
+          </th>
+          <th
+            scope="col"
+            aria-sort={
+              sort.field === 'name.lastName' ? `${sort.order}ending` : 'none'
+            }
+          >
+            {header.name}
+            <SortButton
+              field="name.lastName"
+              label={header.name}
+              onSort={onSort}
+              sort={sort}
+            />
+          </th>
+          <th
+            scope="col"
+            aria-sort={
+              sort.field === 'companyName' ? `${sort.order}ending` : 'none'
+            }
+          >
+            {header.companyName}
+            <SortButton
+              field="companyName"
+              label={header.companyName}
+              onSort={onSort}
+              sort={sort}
+            />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(d => (
+          <Fragment key={d.id}>
             <tr>
-              <th scope="col">{header.id}</th>
-              <th
-                scope="col"
-                onClick={() => {
-                  this.sortIt('name');
-                }}
-              >
-                {header.name}
+              <th className="align-middle" scope="row" rowSpan="2">
+                {d.id}
               </th>
-              <th
-                scope="col"
-                onClick={() => {
-                  this.sortIt('companyName');
-                }}
-              >
-                {header.companyName}
-              </th>
+              <td>{d.name.firstName}</td>
+              <td className="align-middle" rowSpan="2">
+                {d.companyName}
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {data.map(d => (
-              <Fragment key={d.id}>
-                <tr>
-                  <th className="align-middle" scope="row" rowSpan="2">
-                    {d.id}
-                  </th>
-                  <td>{d.name.firstName}</td>
-                  <td className="align-middle" rowSpan="2">
-                    {d.companyName}
-                  </td>
-                </tr>
-                <tr>
-                  <td>{d.name.lastName}</td>
-                </tr>
-              </Fragment>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+            <tr>
+              <td>{d.name.lastName}</td>
+            </tr>
+          </Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default TableCells;
